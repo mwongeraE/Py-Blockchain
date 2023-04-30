4e40227f64188638813c46b284255c600f25bff0
mwongeraE <evansmwongera@gmail.com>
2023-04-30 10:30:57 +0300

    initialize project

---
+++ SimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+
+    uint256 public favoriteNumber;
+    bool favoriteBool;
+
+    // Define new type of object
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Array
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    // People public person = People({favoriteNumber: 2, name: 'Evans'});
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber + favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+}
