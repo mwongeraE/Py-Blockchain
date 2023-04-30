a6d33a737bdc16bedea068f4752712fbf7539beb
mwongeraE <evansmwongera@gmail.com>
2023-04-30 12:37:28 +0300

    ganache setup

--- deploy.py
+++ deploy.py
@@ -1,9 +1,15 @@
 import solcx
 import json
+import os
 
 solcx.install_solc("0.6.0")
 
 from solcx import compile_standard
+from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -32,3 +38,27 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x15454270f2A911e4EC0c686e630F4C1ed08885eb"
+private_key = os.getenv("private_key")
+
+# Create the contract in py
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Get the latest transaction
+nonce = w3.eth.get_transaction_count(my_address)
+print(nonce)
+
+# 1 Build transaction
+# 2 Sign a transaction
+# 3 Send a transaction
+transaction = SimpleStorage.constructor().build_transaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+print(transaction)
